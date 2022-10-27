e40c755ae16bfbe7cccd5e3ff4dfee1df71d05ac
eduardo <eduardo@debian>
2022-10-27 15:44:09 -0300

    Auth2 implementado

--- src/main/java/br/edu/utfpr/paranazom/ParanazomApplication.java
+++ src/main/java/br/edu/utfpr/paranazom/ParanazomApplication.java
@@ -2,8 +2,10 @@ package br.edu.utfpr.paranazom;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 
 @SpringBootApplication
+@EnableAuthorizationServer
 public class ParanazomApplication {
 
 	public static void main(String[] args) {
